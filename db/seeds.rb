da4410304d0df0aba340ecd35f5ac921df26e534
manuelle augereau <manuelleaugereau@gmail.com>
2025-05-28 17:48:15 +0200

    add new/create

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,31 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Modern Flat Paris',
+  address: '10 Clifton Gardens PAris W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 130,
+  number_of_guests: 6
+)
+
+Flat.create!(
+  name: 'Vibrant Flat Berlin',
+  address: '10 Clifton Gardens Berlin W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 70,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Petite chambre dans confortable appartement',
+  address: 'Clichy Paris',
+  description: 'Cet appartement très bien situé est parfait pour circuler dans Paris. Il se trouve dans un immeuble haussmannien au centre du triangle Bastille - Nation - République. Il est pourvu de 3 chambres et d’un vaste salon salle à manger de 30 mètres carrés à partager ainsi que la cuisine et la salle de bains. Il est calme, et confortable. Le métro se trouve à trente mètres de l’entrée de l’immeuble. Il y a une dizaine d’arrêts de bus également à proximité immédiate.',
+  price_per_night: 150,
+  number_of_guests: 2
+)
